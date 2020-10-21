a532775de69ed6b9246d59ebccc605509b5266dc
vladeamear <vladeamear@users.noreply.github.com>
2020-10-21 18:39:30 +0300

    фух, теперь всё

--- src/controller.php
+++ src/controller.php
@@ -1,6 +1,6 @@
 <?php
-  if ($_SERVER['SCRIPT_NAME'] == '/index.php'){
-    printPage('/index.php', $database);
+  if ($_SERVER['SCRIPT_NAME'] == '/index.php'){//если пользователь находится на странице index.php, то
+    printPage('/index.php', $database); //вызывается функция для вывода страницы, в которую передается адрес страницы и массив database
   }
   elseif ($_SERVER['SCRIPT_NAME'] == '/bytovka.php'){
     printPage('/bytovka.php', $database);
@@ -12,18 +12,12 @@
     printPage('/price.php', $database);
   }
 
-  function printPage($url_key, &$database){
-    $data = searchData($database, '/index.php');
-    if (!empty($data) && file_exists(PATH_TPL . $data['tpl'])){
-      include_once(PATH_TPL . $data['tpl']);
-    }else{
-      die('в базе данных нет данных для вызываемой страницы');
-    }
-    $data = searchData($database, '/price.php');
-    if (!empty($data) && file_exists(PATH_TPL . $data['tpl'])){
-      include_once(PATH_TPL . $data['tpl']);
-    }else{
-      die('в базе данных нет данных для вызываемой страницы');
+  function printPage($url_key, &$database){//функция для вывода страницы
+    $data = searchData($database, $url_key);//в переменную data присвваивается информация о странице url_key
+    if (!empty($data) && file_exists(PATH_TPL . $data['tpl'])){//если data не пустая и существует файл-шаблон, соответствующий url_key
+      include_once(PATH_TPL . $data['tpl']);//то этот файл-шаблон включается в страницу
+    }else{//иначе 
+      die('в базе данных нет данных для вызываемой страницы');//скрипт не срабатывает и выводится ошибка из скобок
     }
   }
 ?>
--- src/model.php
+++ src/model.php
@@ -1,8 +1,8 @@
 <?php
-  function searchData(&$database, $url_key){
-    foreach ($database['pages'] as $key => $value){
-      if ($value['url_key'] == $url_key){
-        return $value;
+  function searchData(&$database, $url_key){//функция для поиска информации о страницы из файла database
+    foreach ($database['pages'] as $key => $value){//происходит прогон по массиву pages
+      if ($value['url_key'] == $url_key){//если в массиве pages есть информация об искомой странице, то 
+        return $value; //массив с информацией об этой странице возвращается в программу
       }
     }
     return false;
